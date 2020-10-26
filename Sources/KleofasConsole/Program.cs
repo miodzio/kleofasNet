69d990cc4210faddd8b83709ed148ab26be9a154
Mir <mmiodzio@rmt.vipserv.org>
2020-10-26 13:59:14 +0100

    KLEO-1 first files added

---
+++ Sources/KleofasConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ja jestem Kleofas, Twoj osobisty robot. Jak sie nazywasz?");
+            var imie = Console.ReadLine();
+            Console.WriteLine("Czesc " + imie + " , czy dzisiaj jest wtorek?");
+            var odp1 = Console.ReadKey();
+            Console.WriteLine("");
+
+            if (odp1.KeyChar.Equals('n') || odp1.KeyChar.Equals('N'))
+            {
+                Console.WriteLine(imie + " zgadza sie!");
+            }
+            else
+            {
+                Console.WriteLine(" nie zgadza sie");
+            }
+            Console.ReadLine();
+            Console.WriteLine("pa pa");
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+    }
+}
